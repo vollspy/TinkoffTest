20417b35eed1dbf31bcecade80c0d94b46ae5f28
vollspy <Tolyavetoshkin1337@gmail.com>
2019-02-06 02:37:56 +0500

    On the way.

--- src/test/java/ru/Tinkoff/Pages/HomePage.java
+++ src/test/java/ru/Tinkoff/Pages/HomePage.java
@@ -1,4 +1,21 @@
 package ru.Tinkoff.Pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class HomePage {
+
+    ///Находим ссылку из второго задания, вроде все что нам нужно на первой странице
+    @FindBy(linkText = "Платежи")
+    private WebElement paymentLnk;
+
+    public void click() {
+        this.paymentLnk.click();
+    }
+
+
+
 }
